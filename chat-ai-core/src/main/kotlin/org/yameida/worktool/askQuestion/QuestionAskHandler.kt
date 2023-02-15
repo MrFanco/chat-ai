29eb0d0850f2bbc3bb7e62268d9ed6af48f1b639
李博林 <libolin@aegis-data.cn>
2023-02-15 18:29:32 +0800

    add: 提升性能

--- chat-ai-core/src/main/kotlin/org/yameida/worktool/askQuestion/QuestionAskHandler.kt
+++ chat-ai-core/src/main/kotlin/org/yameida/worktool/askQuestion/QuestionAskHandler.kt
@@ -11,6 +11,7 @@ import org.yameida.worktool.common.event.AnswerQuestionEvent
 import org.yameida.worktool.common.event.DataUpdateEvent
 import org.yameida.worktool.common.utils.MsgUtil
 import org.yameida.worktool.common.utils.SpringUtils
+import org.yameida.worktool.common.utils.ThreadPoolManager
 import org.yameida.worktool.data.service.ChatQuestionService
 import org.yameida.worktool.service.AIService
 import org.yameida.worktool.service.QuestionService
@@ -41,8 +42,8 @@ open class QuestionAskHandler : BaseHandler("消息询问线程") {
             return
         }
         //如果消息重试次数大于4次就丢弃
-        if (question.reTryCount > 4) {
-            logger.info("消息重试次数大于4词,丢弃 ${JSONObject.toJSONString(question)} 秒钟")
+        if (question.reTryCount > 8) {
+            logger.info("消息重试次数大于8次,丢弃 ${JSONObject.toJSONString(question)} 秒钟")
             return
         }
 
@@ -55,7 +56,6 @@ open class QuestionAskHandler : BaseHandler("消息询问线程") {
         //获取上次询问时间
         val lastTime = questionCore().getAIUseAITime(ai.id)
 
-
         //判断当前机器人能否使用 基础间隔时间未5秒 随机打样
         if (Random().nextBoolean() && askTime - lastTime > 5000) {
             logger.info("ai id为 :${ai.id} 暂时不能使用,消息重新加入到消息队列中 ")
@@ -64,17 +64,19 @@ open class QuestionAskHandler : BaseHandler("消息询问线程") {
             sleep(READ_MSG_TIME_INTERVAL)
             return
         }
-        question.answer = aIService().getAnswer(question.spoken, ai)
-        //询问时间
-        val answerTime = System.currentTimeMillis();
-        question.costTime = (answerTime - askTime).toInt()
-
-        //产生新的消息体
-        val msg = MsgUtil.initMsg(question.answer, question.groupRemark, question.groupName, question.receivedName)
-        SpringUtils.getApplicationContext().publishEvent(AnswerQuestionEvent(question.robotId, msg))
-
-        //更新问题数据库
-        SpringUtils.getApplicationContext().publishEvent(DataUpdateEvent(question, ChatQuestionService::class.java))
+        val answerTime = System.currentTimeMillis()
+        ThreadPoolManager.getInstance().execute {
+            question.answer = aIService().getAnswer(question.spoken, ai)
+            //询问时间
+            logger.info("ai编号:${ai.id} 消息询问中-----------------------")
+            question.costTime = (answerTime - askTime).toInt()
+            logger.info("ai编号:${ai.id} 消息询结束-----------------------")
+            //产生新的消息体
+            val msg = MsgUtil.initMsg(question.answer, question.groupRemark, question.groupName, question.receivedName)
+            SpringUtils.getApplicationContext().publishEvent(AnswerQuestionEvent(question.robotId, msg))
+            //更新问题数据库
+            SpringUtils.getApplicationContext().publishEvent(DataUpdateEvent(question, ChatQuestionService::class.java))
+        }
         //更新AI使用时间
         questionCore().updateAIUseTime(ai.id, answerTime)
         sleep(READ_MSG_TIME_INTERVAL)
@@ -87,7 +89,6 @@ open class QuestionAskHandler : BaseHandler("消息询问线程") {
     open fun aIService(): AIService {
         return SpringUtils.getBean(AIService::class.java);
     }
-
 
 
     open fun questionCore(): QuestionCore {
